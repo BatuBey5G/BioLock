35c530d20ec9a0f65258e501acf100ece53e639b
BatuBey5G <158512172+BatuBey5G@users.noreply.github.com>
2026-02-06 21:42:53 +0300

    tweak

---
+++ BioLock/biolockprefs/BLPRootListController.h
@@ -0,0 +1,10 @@
+#import <Preferences/PSListController.h>
+
+@interface BLPRootListController : PSListController
+
+- (void)clearAuthCache:(id)sender;
+- (void)resetSettings:(id)sender;
+- (void)openGitHub:(id)sender;
+- (void)showCompletionAlert:(NSString *)message;
+
+@end
